4c5c0b66d0dd4fd031c8400aed5439852a331888
e1bandito <aleksandr.shitov@mail.ru>
2021-08-07 13:52:54 +0300

    Update 2021-08-07T10:52:49.715Z

--- src/js/import/main.js
+++
@@ -1,9 +0,0 @@
-// require('../../blocks/modal/modal');
-
-import burger from "../../blocks/burger/burger";
-import {mainNavClose} from "../../blocks/main-nav/main-nav";
-import {toggleDropdown} from "../../blocks/main-nav/main-nav"
-
-burger('.burger', '.main-nav');
-mainNavClose();
-toggleDropdown();
